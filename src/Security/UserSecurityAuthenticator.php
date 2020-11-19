ee371ae3ead39561c16fa81e8f011829d4638e17
mathieu raimbault <mathieu.raimbault@hotmail.com>
2020-11-19 12:02:10 +0100

    :bug: Fix registration

--- src/Security/UserSecurityAuthenticator.php
+++ src/Security/UserSecurityAuthenticator.php
@@ -20,7 +20,7 @@ use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticato
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class UserAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
+class UserSecurityAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
     use TargetPathTrait;
 
